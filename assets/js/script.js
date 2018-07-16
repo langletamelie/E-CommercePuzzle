f3190798d1129c3d2cd94314242046167b1970fa
langletamelie <amelie.langlet@hotmail.com>
2018-07-16 14:48:25 +0200

    ajout partie Manu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
 var app = angular.module('appPuzzle', []);
 app.controller('displayContent', function($scope, $http) {
   $http.get('assets/js/puzzle.json').then(function(response) {
-      $scope.puzzles = response.data;
+    $scope.puzzles = response.data;
   });
+  $scope.changeCategory = function(category) {
+    $scope.filterChange = category;
+  };
+    $scope.changeOrder = function(order) {
+      $scope.orderby = order;
+  };
+  var quantityBasket = [];
+  $scope.addBasket = function(add) {
+    var id = add;
+    if(!(id in quantityBasket)){
+      quantityBasket[id] = 1;
+    }else{
+      quantityBasket[id]++;
+    }
+  }
 });
